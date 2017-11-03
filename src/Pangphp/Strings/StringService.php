bc682c71d3e6a763831c9f1783b6712afb3307d5
Byron Georgiou <byron@isarray.com>
2017-11-03 17:44:17 -0400

    Added a more random funciton to random string

--- src/Pangphp/Strings/StringService.php
+++ src/Pangphp/Strings/StringService.php
@@ -5,19 +5,12 @@ namespace Pangphp\Strings;
 class StringService {
 
   function randomStringGenerator($int) {
+    $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $max = mb_strlen($keyspace, '8bit') - 1;
     $str = "";
-
-    for($i = 0; $i < $int; $i++) {
-        $rand = rand(0, 2);
-        $caps = chr(rand(65, 90));
-        $nums = chr(rand(48, 57));
-        $lows = chr(rand(97, 122));
-        
-        $array = array($caps, $nums, $lows);
-        
-        $str .= $array[$rand];
+    for ($i = 0; $i < $int; ++$i) {
+        $str .= $keyspace[random_int(0, $max)];
     }
-    
     return $str;
   }
 
